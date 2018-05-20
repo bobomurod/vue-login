7f51811f5e675cdb745ac8c05b0e4b8423a91eb0
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-20 10:31:09 +0300

    first commit

---
+++ src/views/secure.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="secure">
+        <h1>Secure area</h1>
+        <p>
+            This is secure area
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Secure',
+    data() {
+        return {};
+    }
+}
+</script>
+
+<style scoped>
+    #secure {
+        background-color: beige;
+        border: 1px solid #cccccc;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
+
+
